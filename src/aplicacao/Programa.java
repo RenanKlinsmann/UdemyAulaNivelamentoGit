5af287f8e10b494b963d548868bef2e77f5cf877
Renan Klinsmann <klismann-nan@hotmail.com>
2020-03-23 17:32:52 -0300

    Acrestada uma pergunta como voce esta?

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,7 +5,7 @@ public class Programa {
 	public static void main(String[] args) {
 		System.out.println("Good morning!");
 		System.out.println("Good night!");
-		
+		System.out.println("How are you?");
 
 	}
 
